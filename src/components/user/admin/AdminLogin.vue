ec8709ed79dc8c8dd32fef8ddd08df07927091e3
yoeaki <1743182950@qq.com>
2020-05-23 20:51:59 +0800

    1.更新代码

--- src/components/user/admin/AdminLogin.vue
+++ src/components/user/admin/AdminLogin.vue
@@ -38,7 +38,7 @@ export default {
                 password: '',
             },
             rules: {
-                username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+                name: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
                 password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
             },
         };
